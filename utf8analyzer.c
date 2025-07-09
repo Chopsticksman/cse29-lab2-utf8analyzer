7e382ad066ecf0115a8209d7cb7c896f89a56100
Geller <cgeller@ieng6-202.ucsd.edu>
2025-07-09 16:05:25 -0700

    Cole (2)

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -10,5 +10,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    
+void upper_case(char input[])
+{
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		if (input[i] >= 'a' & input[i] <= 'z')
+	{
+		input[i] -= 32;
+	}
+	else
+	{
+
+return input[];    
+    
+    
+    
+    
+    
+    
+    
+    
     // implement the UTF-8 analyzer here
 }
